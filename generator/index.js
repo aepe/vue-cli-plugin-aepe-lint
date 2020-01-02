de85b022b3b18af36004efb776958c40965e4cd7
bhabgs <bhabgs@aliyun.com>
2020-01-02 15:12:29 +0800

    修改依赖关系

--- generator/index.js
+++ generator/index.js
@@ -4,17 +4,17 @@
  * @Author: bhabgs
  * @Date: 2020-01-02 09:34:41
  * @LastEditors  : bhabgs
- * @LastEditTime : 2020-01-02 13:50:45
+ * @LastEditTime : 2020-01-02 15:10:04
  */
 module.exports = (api, options, rootOptions) => {
-  api.extendPackage({
-    dependencies: {
-      // prettier-ignore
-      "prettier": "^1.19.1",
-      "eslint-config-prettier": "^6.8.0",
-      "eslint-plugin-prettier": "^3.1.2",
-      "eslint-plugin-vue": "6.1.2"
-    }
-  });
+  //   api.extendPackage({
+  //     dependencies: {
+  //       // prettier-ignore
+  //       "prettier": "^1.19.1",
+  //       "eslint-config-prettier": "^6.8.0",
+  //       "eslint-plugin-prettier": "^3.1.2",
+  //       "eslint-plugin-vue": "6.1.2"
+  //     }
+  //   });
   api.render("./template");
 };
