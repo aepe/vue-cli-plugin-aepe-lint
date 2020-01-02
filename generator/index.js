b0b7baad9e705872be57ad16326bfa05fb72deab
bhabgs <bhabgs@aliyun.com>
2020-01-02 15:40:33 +0800

    修改依赖关系

--- generator/index.js
+++ generator/index.js
@@ -4,11 +4,11 @@
  * @Author: bhabgs
  * @Date: 2020-01-02 09:34:41
  * @LastEditors  : bhabgs
- * @LastEditTime : 2020-01-02 15:35:16
+ * @LastEditTime : 2020-01-02 15:40:13
  */
 module.exports = (api, options, rootOptions) => {
   api.extendPackage({
-    dependencies: {
+    devDependencies: {
       // prettier-ignore
       "prettier": "^1.19.1",
       "eslint-config-airbnb-base": "^14.0.0",
