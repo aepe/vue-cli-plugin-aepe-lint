686d5c78bb4885a28a7e969ed9e99e39090a23c3
bhabgs <bhabgs@aliyun.com>
2020-01-02 15:46:17 +0800

    更新版本

--- generator/index.js
+++ generator/index.js
@@ -4,7 +4,7 @@
  * @Author: bhabgs
  * @Date: 2020-01-02 09:34:41
  * @LastEditors  : bhabgs
- * @LastEditTime : 2020-01-02 15:44:50
+ * @LastEditTime : 2020-01-02 15:46:03
  */
 module.exports = (api, options, rootOptions) => {
   api.extendPackage({
@@ -15,7 +15,9 @@ module.exports = (api, options, rootOptions) => {
       "eslint-config-prettier": "^6.9.0",
       "eslint-plugin-prettier": "^3.1.2",
       "eslint-plugin-vue": "^6.1.2",
-      "eslint-plugin-import": "^2.19.1"
+      "eslint-plugin-import": "^2.19.1",
+      "@typescript-eslint/eslint-plugin": "^2.14.0",
+      "@typescript-eslint/parser": "^2.14.0"
     }
   });
   api.extendPackage({
