d0cbe5080181fb179d0254f4566cf24ee6d5a995
bhabgs <bhabgs@aliyun.com>
2020-01-02 11:38:35 +0800

    修改模板

--- generator/index.js
+++ generator/index.js
@@ -4,7 +4,7 @@
  * @Author: bhabgs
  * @Date: 2020-01-02 09:34:41
  * @LastEditors  : bhabgs
- * @LastEditTime : 2020-01-02 10:33:10
+ * @LastEditTime : 2020-01-02 11:08:22
  */
 module.exports = (api, options, rootOptions) => {
   api.extendPackage({
@@ -12,7 +12,8 @@ module.exports = (api, options, rootOptions) => {
       // prettier-ignore
       "prettier": "^1.19.1",
       "eslint-config-prettier": "^6.8.0",
-      "eslint-plugin-prettier": "^3.1.2"
+      "eslint-plugin-prettier": "^3.1.2",
+      "eslint-plugin-vue": "6.1.2"
     }
   });
   api.render("./template");
--- generator/template/_eslintrc.js
+++ generator/template/_eslintrc.js
@@ -3,58 +3,55 @@
  * @version: 0.0.1
  * @Author: bhabgs
  * @Date: 2020-01-02 10:12:53
- * @LastEditors: bhabgs
- * @LastEditTime: 2020-01-02 10:23:21
+ * @LastEditors  : bhabgs
+ * @LastEditTime : 2020-01-02 11:07:46
  */
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true
-  },
-  extends: ["airbnb-base", "plugin:prettier/recommended"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
-  },
-  root: true,
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-      modules: true
-    }
-  },
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx"],
-      plugins: ["@typescript-eslint"],
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        project: "./tsconfig.json",
-        tsconfigRootDir: __dirname,
-        experimentalDecorators: true
-      },
-      rules: {
-        "@typescript-eslint/consistent-type-definitions": [
-          "error",
-          "interface"
-        ],
-        "@typescript-eslint/explicit-function-return-type": "error",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/typedef": [
-          "error",
-          {
-            variableDeclaration: true
-          }
-        ]
-      }
-    }
-  ],
-  rules: {
-    "prettier/prettier": "error",
-    "no-console": "off",
-    "array-callback-return": "off"
-  }
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: ['airbnb-base', 'plugin:prettier/recommended', 'eslint-plugin-vue'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    root: true,
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+            modules: true,
+        },
+    },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            plugins: ['@typescript-eslint'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                project: './tsconfig.json',
+                tsconfigRootDir: __dirname,
+                experimentalDecorators: true,
+            },
+            rules: {
+                '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+                '@typescript-eslint/explicit-function-return-type': 'error',
+                '@typescript-eslint/no-explicit-any': 'off',
+                '@typescript-eslint/typedef': [
+                    'error',
+                    {
+                        variableDeclaration: true,
+                    },
+                ],
+            },
+        },
+    ],
+    rules: {
+        'prettier/prettier': 'error',
+        'no-console': 'off',
+        'array-callback-return': 'off',
+    },
 };
