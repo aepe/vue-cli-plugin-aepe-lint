461ba852ac60de8a86b544401950036229cdd881
bhabgs <bhabgs@aliyun.com>
2020-01-09 11:21:32 +0800

    取消重复引入的解析器

--- generator/template/_eslintrc.js
+++ generator/template/_eslintrc.js
@@ -4,98 +4,29 @@
  * @Author: bhabgs
  * @Date: 2020-01-02 10:12:53
  * @LastEditors  : bhabgs
- * @LastEditTime : 2020-01-07 09:31:55
+ * @LastEditTime : 2020-01-09 11:17:51
  */
-const path = require("path");
+const { vue, ts, rules } = require("./eslintconfig");
+
 module.exports = {
   env: {
     browser: true,
     es6: true,
     node: true
   },
-  extends: [
-    "airbnb-base",
-    "plugin:vue/essential",
-    "plugin:prettier/recommended"
-  ],
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
   },
   root: true,
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 8,
     sourceType: "module",
     ecmaFeatures: {
-      jsx: true,
-      modules: true
+      jsx: true
     }
   },
-  overrides: [
-    {
-      files: ["*.vue"],
-      parser: "vue-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser"
-      },
-      rules: {
-        "vue/singleline-html-element-content-newline": [
-          "error",
-          {
-            ignoreWhenNoAttributes: true,
-            ignoreWhenEmpty: true,
-            ignores: ["pre", "textarea"]
-          }
-        ],
-        "vue/html-closing-bracket-newline": [
-          "error",
-          {
-            singleline: "never",
-            multiline: "always"
-          }
-        ],
-        "vue/html-self-closing": [
-          "error",
-          {
-            html: {
-              void: "never",
-              normal: "always",
-              component: "always"
-            },
-            svg: "always",
-            math: "always"
-          }
-        ]
-      }
-    },
-    {
-      files: ["*.ts", "*.tsx"],
-      plugins: ["@typescript-eslint"],
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        project: "./tsconfig.json",
-        tsconfigRootDir: __dirname,
-        experimentalDecorators: true,
-        ecmaFeatures: {
-          legacyDecorators: true
-        }
-      },
-      rules: {
-        "@typescript-eslint/semi": ["error"],
-        "@typescript-eslint/indent": ["error", 2],
-        "@typescript-eslint/explicit-function-return-type": 0
-      }
-    }
-  ],
-  settings: {},
-  rules: {
-    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0, // 打包时禁止debugger
-    "no-console": process.env.NODE_ENV === "production" ? 2 : 0, // 打包时禁止console
-    "no-alert": process.env.NODE_ENV === "production" ? 2 : 0, // 打包时禁止console
-    "no-unused-vars": 1,
-    "no-var": 1,
-    "class-methods-use-this": 0,
-    "import/no-unresolved": 0,
-    "import/extensions": 0
-  }
+  overrides: [vue, ts],
+  rules
 };
---
+++ generator/template/eslintconfig/file-rules.js
@@ -0,0 +1,43 @@
+/*
+ * @abstract: JianJie
+ * @version: 0.0.1
+ * @Author: bhabgs
+ * @Date: 2020-01-09 10:06:56
+ * @LastEditors  : bhabgs
+ * @LastEditTime : 2020-01-09 11:16:03
+ */
+const vueRules = {
+  'vue/singleline-html-element-content-newline': 0,
+  'vue/html-closing-bracket-newline': 0,
+};
+
+exports.vue = {
+  files: ['*.vue'],
+  excludedFiles: ['*.ts', '*.tsx'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2017,
+    sourceType: 'module',
+  },
+  rules: vueRules,
+};
+
+exports.ts = {
+  files: ['*.ts', '*.tsx'],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: '../tsconfig.json',
+    tsconfigRootDir: __dirname,
+    experimentalDecorators: true,
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
+  },
+  rules: {
+    '@typescript-eslint/semi': ['error'],
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/explicit-function-return-type': 0,
+  },
+};
