f7bc121e968bb4085d572af119bcca6cf520dc5f
张鑫 <zhangxin@tjmeiteng.com>
2020-04-26 15:00:48 +0800

    添加zip打包方案

--- generator/template/vueconf/configureWebpack.js
+++ generator/template/vueconf/configureWebpack.js
@@ -4,31 +4,52 @@
  * @Author: bhabgs
  * @Date: 2020-04-14 16:25:33
  * @LastEditors: bhabgs
- * @LastEditTime: 2020-04-14 16:27:40
+ * @LastEditTime: 2020-04-26 14:59:47
  */
-const StatsPlugin = require('stats-webpack-plugin');
-const VueConf = require('./index').default;
+const StatsPlugin = require("stats-webpack-plugin");
+const FileManagerPlugin = require("filemanager-webpack-plugin");
+const VueConf = require("./index").default;
+
+const plugins = [
+  new StatsPlugin("manifest.json", {
+    // 方便后期父项目动态加载
+    chunkModules: false,
+    entrypoints: true,
+    source: false,
+    chunks: false,
+    modules: false,
+    assets: false,
+    children: false,
+    exclude: [/node_modules/],
+  }),
+];
+
+const buildZip = new FileManagerPlugin({
+  onEnd: {
+    delete: [`./dist/${VueConf.library}.zip`],
+    archive: [
+      {
+        source: `./dist`,
+        destination: `./dist/${VueConf.library}.zip`,
+      },
+    ],
+  },
+});
+
+// 当命令为build时，进行zip打包模式
+if (VueConf.isProd) {
+  plugins.push(buildZip);
+}
+
 exports.default = {
-  devtool: 'none', // 不打包sourcemap
+  devtool: "none", // 不打包sourcemap
   output: {
     library: VueConf.library, // 导出名称
-    libraryTarget: 'window', //挂载目标
+    libraryTarget: "window", // 挂载目标
   },
   performance: {
     hints: false,
   },
-  externals: VueConf.externals,
-  plugins: [
-    new StatsPlugin('manifest.json', {
-      // 方便后期父项目动态加载
-      chunkModules: false,
-      entrypoints: true,
-      source: false,
-      chunks: false,
-      modules: false,
-      assets: false,
-      children: false,
-      exclude: [/node_modules/],
-    }),
-  ],
+  externals: process.env.INTERNET === "intranet" ? {} : VueConf.externals,
+  plugins,
 };
--- generator/template/vueconf/index.js
+++ generator/template/vueconf/index.js
@@ -2,43 +2,43 @@
  * @abstract: JianJie
  * @version: 0.0.1
  * @Author: bhabgs
- * @Date: 2020-04-14 09:55:34
+ * @Date: 2020-04-14 17:18:48
  * @LastEditors: bhabgs
- * @LastEditTime: 2020-04-14 17:17:34
+ * @LastEditTime: 2020-04-26 15:00:08
  */
-const package = require('../package.json');
+const package = require("../package.json");
 
 const assetsCDN = {
   externals: {
-    vue: 'Vue',
-    'vue-router': 'VueRouter',
-    'ant-design-vue': 'antd',
-    vuex: 'Vuex',
-    axios: 'axios',
-    moment: 'moment',
+    vue: "Vue",
+    "vue-router": "VueRouter",
+    "ant-design-vue": "antd",
+    vuex: "Vuex",
+    axios: "axios",
+    moment: "moment",
   },
-  css: ['https://cdn.jsdelivr.net/npm/ant-design-vue@1.4.12/dist/antd.css'],
+  css: ["https://cdn.jsdelivr.net/npm/ant-design-vue@1.4.12/dist/antd.css"],
   js: [
-    '//cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
-    '//cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js',
-    '//cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js',
-    '//cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js',
-    'https://cdn.jsdelivr.net/npm/moment@2.24.0/min/moment.min.js',
-    'https://cdn.jsdelivr.net/npm/ant-design-vue@1.4.12/dist/antd.min.js',
+    "//cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js",
+    "//cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js",
+    "//cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js",
+    "//cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js",
+    "https://cdn.jsdelivr.net/npm/moment@2.24.0/min/moment.min.js",
+    "https://cdn.jsdelivr.net/npm/ant-design-vue@1.4.12/dist/antd.min.js",
   ],
 };
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 const conf = {
   isProd,
-  title: 'template',
-  library: 'micro-template',
-  publicPath: 'micro-template',
+  title: "template",
+  library: "micro-template",
+  publicPath: "/micro-template",
 };
 
 const chainWebpack = (config) => {
-  config.plugin('html').tap((args) => {
+  config.plugin("html").tap((args) => {
     cdn = {
       css: assetsCDN.css,
     };
@@ -50,13 +50,19 @@ const chainWebpack = (config) => {
     if (isProd) {
       args[0] = Object.assign(args[0], { cdn: assetsCDN });
     }
+
+    if (process.env.INTERNET === "intranet") {
+      args[0].cdn = {};
+    }
+
     return args;
   });
 };
 
 exports.default = {
-  library: '',
-  publicPath: '',
+  library: conf.library,
+  publicPath: conf.publicPath,
   chainWebpack,
+  isProd,
   externals: isProd ? assetsCDN.externals : {},
 };
